20a2d1beee2ebd6e38a8a741513ec4eb21f76962
Divyanshu Srivastava <112887868+Divyanshu1712@users.noreply.github.com>
2026-01-19 09:25:26 +0530

    ok

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "./components/common/theme-provider"
+import Dashboard from "./pages/Dashboard"
+
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
+
+export default App
