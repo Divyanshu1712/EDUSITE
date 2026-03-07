42583fae916d3f80216061626d9e116398258324
Divyanshu <divyanshu.srivastava@botmantra.com>
2026-03-07 14:21:53 +0530

    'check'

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,10 +8,10 @@ class User(Base):
     __tablename__  = "user_master"
     
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    email = Column(String(150), unique=True, nullable=False)
+    email = Column(String(150), unique=True, nullable=False, index=True)
     password_hash = Column(String(255), nullable=False)
     full_name = Column(String(100), nullable=False)
     role = Column(String(20), nullable=False)
     user_status = Column(String(10), nullable=False, default="ACTIVE")
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    
+
--- backend/users/router.py
+++ backend/users/router.py
@@ -52,3 +52,15 @@ def delete_user(
     db.delete(user)
     db.commit()
     return {"message": "User deleted successfully"}
+
+
+def upadteUser(
+    user_id: str,
+    db: Session = Depends(get_db),
+    admin = Depends(require_admin)
+    ):
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    
